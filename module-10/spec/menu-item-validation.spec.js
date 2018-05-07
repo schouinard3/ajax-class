6a38da35f61764fe5b07e6776a0d930e44c00cb3
Stephanie Chouinard <(none)>
2018-05-06 20:34:58 -0400

    Updated spec with new test code - double checked the expected validity values and put the flush call before the expects.

--- module-10/spec/menu-item-validation.spec.js
+++ module-10/spec/menu-item-validation.spec.js
@@ -1,26 +1,46 @@
 describe('validate-menu-shortname validation directive', function() {
 
-    var validationDirective;
     var $httpBackend;
     var RestaurantServerUrl;
+
     var $compile;
     var $rootScope;
-    var formTemplate;
+    var formHtml;
 
-    beforeEach(function() {
-        module('restaurant');
-        inject(function($injector) {
-            $httpBackend = $injector.get('$httpBackend');
-            RestaurantServerUrl = $injector.get('RestaurantServerUrl');
-        });
-        inject(function(_$rootScope_, _$compile_) {
-            $rootScope = _$rootScope_;
-            $compile = _$compile_;
-        });
-    });
+    beforeEach(module('restaurant'));
+
+    beforeEach(inject(function($injector) {
+        $httpBackend = $injector.get('$httpBackend');
+        RestaurantServerUrl = $injector.get('RestaurantServerUrl');
+    }));
 
     beforeEach(inject(function($templateCache) {
+        var publicTemplate = null;
+        var publicTemplateRequest = new XMLHttpRequest();
+        publicTemplateRequest.onload = function() {
+            publicTemplate = this.responseText;
+        };
+        publicTemplateRequest.open("get", "src/public/public.html", false);
+        $templateCache.put("src/public/public.html", publicTemplate);
 
+        var homeTemplate = null;
+        var homeTemplateRequest = new XMLHttpRequest();
+        homeTemplateRequest.onload = function() {
+            homeTemplate = this.responseText;
+        };
+        homeTemplateRequest.open("get", "src/public/home/home.html", false);
+        $templateCache.put("src/public/home/home.html", homeTemplate);
+    }));
+
+    beforeEach(inject(function(_$rootScope_, _$compile_) {
+        $rootScope = _$rootScope_;
+        $compile = _$compile_;
+        formHtml = angular.element("<form name='testform'> \
+            <input type='text' \
+                name='dish' \
+                ng-model='testmodel.dish' \
+                validate-menu-shortname /> \
+        </form>");
     }));
 
     var successfulObj = {
@@ -38,19 +58,42 @@ describe('validate-menu-shortname validation directive', function() {
         "image_present": false
     };
 
-    xit('should return dish information', function() {
-        var controllerObj = {
-            signupCtrl: {
-                user: {
-                    firstName: 'first',
-                    lastName: 'last',
-                    email: 'first@last',
-                    phone: '555-555-5555',
-                    dish: 'YES'
-                }
-            }
-        };
-        $httpBackend.whenGET(RestaurantServerUrl+'/menu_items/YES.json').respond(successfulObj);
+    it('should validate true for valid dishes', function() {
+        $httpBackend.whenGET(RestaurantServerUrl+'/menu_items/YES.json').respond(200, successfulObj);
+        $rootScope.testmodel = { dish: 'YES' };
+        $compile(formHtml)($rootScope);
+
+        var form = $rootScope.testform;
+        $rootScope.$digest();
         $httpBackend.flush();
+
+        expect($rootScope.testmodel.dish).toEqual(successfulObj.short_name);
+        expect($rootScope.testform.dish.$valid).toBe(true);
+    });
+
+    it('should validate false for invalid dishes', function() {
+        $httpBackend.whenGET(RestaurantServerUrl+'/menu_items/NO.json').respond(404, undefined);
+        $rootScope.testmodel = { dish: 'NO' };
+        $compile(formHtml)($rootScope);
+
+        var form = $rootScope.testform;
+        $rootScope.$digest();
+        $httpBackend.flush();
+
+        expect($rootScope.testmodel.dish).toEqual('NO');
+        expect($rootScope.testform.dish.$valid).toBe(false);
+    });
+
+    it('should validate false for server error response codes', function() {
+        $httpBackend.whenGET(RestaurantServerUrl+'/menu_items/error.json').respond(500, undefined);
+        $rootScope.testmodel = { dish: 'error' };
+        $compile(formHtml)($rootScope);
+
+        var form = $rootScope.testform;
+        $rootScope.$digest();
+        $httpBackend.flush();
+
+        expect($rootScope.testmodel.dish).toEqual('error');
+        expect($rootScope.testform.dish.$valid).toBe(false);
     });
 });
